e981f370ff8a0b46ff976edfbb7d8c032484cf8d
matheustristao <matheus.tristao@gmail.com>
2015-04-10 00:39:20 -0300

    envio de dados do formulario pro server php

--- app/src/main/java/com/unb/gerenciadorbicicletaeletrica/MainActivity.java
+++ app/src/main/java/com/unb/gerenciadorbicicletaeletrica/MainActivity.java
@@ -1,39 +1,76 @@
 package com.unb.gerenciadorbicicletaeletrica;
 
-import android.support.v7.app.ActionBarActivity;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void enviarDados(View view){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+        new Thread(){
+            public void run(){
+                EditText nEt = (EditText) findViewById(R.id.nome);
+                EditText sEt = (EditText) findViewById(R.id.sobrenome);
+                EditText eEt = (EditText) findViewById(R.id.email);
+
+                postHttp(nEt.getText().toString(), sEt.getText().toString(), eEt.getText().toString());
+            }
+        }.start();
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void postHttp(String nome, String sobrenome, String email){
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost("http://192.168.0.18/server_bike/server.php");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        try{
+            ArrayList<NameValuePair> valores = new ArrayList<NameValuePair>();
+            valores.add(new BasicNameValuePair("nome", nome));
+            valores.add(new BasicNameValuePair("sobrenome", sobrenome));
+            valores.add(new BasicNameValuePair("email", email));
+
+            httpPost.setEntity(new UrlEncodedFormEntity(valores));
+            final HttpResponse resposta = httpClient.execute(httpPost);
+
+            runOnUiThread(new Runnable(){
+                public void run(){
+                    try {
+                        Toast.makeText(getBaseContext(), EntityUtils.toString(resposta.getEntity()), Toast.LENGTH_SHORT).show();
+                    } catch (ParseException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
-
-        return super.onOptionsItemSelected(item);
+        catch(ClientProtocolException e){}
+        catch(IOException e){}
     }
 }
---
+++ server_bike_php/server.php
@@ -0,0 +1,10 @@
+<?php
+
+	
+	$nome = $_POST['nome'];
+	$sobrenome = $_POST['sobrenome'];
+	$email = $_POST['email'];
+	
+
+	echo 'Variavel extraidas do server PHP e nao do android: Nome: ' .$nome. '  !!!'
+?>
