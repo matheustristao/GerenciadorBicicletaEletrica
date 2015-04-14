d4a22e0c70b7081c2cc315f18780a7a28a1965e2
matheus <matheus.tristao@gmail.com>
2015-04-14 00:05:15 -0300

    Menu inicial

---
+++ app/src/main/java/com/unb/gerenciadorbicicletaeletrica/Cadastro.java
@@ -0,0 +1,91 @@
+package com.unb.gerenciadorbicicletaeletrica;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Cadastro extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+
+    public void enviarDados(View view){
+
+
+        new Thread(){
+            public void run(){
+                EditText nomeEt = (EditText) findViewById(R.id.nome);
+                EditText sobrenomeEt = (EditText) findViewById(R.id.sobrenome);
+                EditText emailEt = (EditText) findViewById(R.id.email);
+                EditText telefoneEt = (EditText) findViewById(R.id.telefone);
+                EditText senhaEt = (EditText) findViewById(R.id.senha);
+                EditText repeatsenhatEt = (EditText) findViewById(R.id.repeat_senha);
+
+                if(senhaEt.getText().toString().equals(repeatsenhatEt.getText().toString())){
+                    postHttp(nomeEt.getText().toString(), sobrenomeEt.getText().toString(), emailEt.getText().toString(), telefoneEt.getText().toString(),senhaEt.getText().toString());
+                }
+                else {
+                    Cadastro.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(Cadastro.this, "Senhas não batem. Digite novamente", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+
+        }.start();
+
+    }
+
+    public void postHttp(String nome, String sobrenome, String email, String telefone, String senha){
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost("http://192.168.0.18/xampp/server_bike/server.php");
+
+        try{
+            ArrayList<NameValuePair> valores = new ArrayList<NameValuePair>();
+            valores.add(new BasicNameValuePair("nome", nome));
+            valores.add(new BasicNameValuePair("sobrenome", sobrenome));
+            valores.add(new BasicNameValuePair("email", email));
+            valores.add(new BasicNameValuePair("telefone", telefone));
+            valores.add(new BasicNameValuePair("senha", senha));
+
+            httpPost.setEntity(new UrlEncodedFormEntity(valores));
+            final HttpResponse resposta = httpClient.execute(httpPost);
+
+            runOnUiThread(new Runnable(){
+                public void run(){
+                    try {
+                        Toast.makeText(getBaseContext(), EntityUtils.toString(resposta.getEntity()), Toast.LENGTH_SHORT).show();
+                    } catch (ParseException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        catch(ClientProtocolException e){}
+        catch(IOException e){}
+    }
+}
--- app/src/main/java/com/unb/gerenciadorbicicletaeletrica/MainActivity.java
+++ app/src/main/java/com/unb/gerenciadorbicicletaeletrica/MainActivity.java
@@ -15,9 +15,11 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -26,66 +28,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void enviarDados(View view){
+        Button btNovoUsuario;
 
+        btNovoUsuario = (Button) findViewById(R.id.btNovoUsuario);
 
-        new Thread(){
-            public void run(){
-                EditText nomeEt = (EditText) findViewById(R.id.nome);
-                EditText sobrenomeEt = (EditText) findViewById(R.id.sobrenome);
-                EditText emailEt = (EditText) findViewById(R.id.email);
-                EditText telefoneEt = (EditText) findViewById(R.id.telefone);
-                EditText senhaEt = (EditText) findViewById(R.id.senha);
-                EditText repeatsenhatEt = (EditText) findViewById(R.id.repeat_senha);
-
-                if(senhaEt.getText().toString().equals(repeatsenhatEt.getText().toString())){
-                    postHttp(nomeEt.getText().toString(), sobrenomeEt.getText().toString(), emailEt.getText().toString(), telefoneEt.getText().toString(),senhaEt.getText().toString());
-                }
-                else {
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(MainActivity.this, "Senhas não batem. Digite novamente", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-                }
-
-        }.start();
+        btNovoUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), Cadastro.class);
+                startActivity(i);
+            }
+        });
 
     }
 
-    public void postHttp(String nome, String sobrenome, String email, String telefone, String senha){
-        HttpClient httpClient = new DefaultHttpClient();
-        HttpPost httpPost = new HttpPost("http://192.168.0.18/xampp/server_bike/server.php");
 
-        try{
-            ArrayList<NameValuePair> valores = new ArrayList<NameValuePair>();
-            valores.add(new BasicNameValuePair("nome", nome));
-            valores.add(new BasicNameValuePair("sobrenome", sobrenome));
-            valores.add(new BasicNameValuePair("email", email));
-            valores.add(new BasicNameValuePair("telefone", telefone));
-            valores.add(new BasicNameValuePair("senha", senha));
-
-            httpPost.setEntity(new UrlEncodedFormEntity(valores));
-            final HttpResponse resposta = httpClient.execute(httpPost);
-
-            runOnUiThread(new Runnable(){
-                public void run(){
-                    try {
-                        Toast.makeText(getBaseContext(), EntityUtils.toString(resposta.getEntity()), Toast.LENGTH_SHORT).show();
-                    } catch (ParseException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-        catch(ClientProtocolException e){}
-        catch(IOException e){}
-    }
 }
